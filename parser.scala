22e30e0e32b26608701a108902a9218af8a76230
Dani Dickstein <danidickstein@yahoo.com>
2015-02-16 16:41:28 -0500

    made the parser use packrat

--- parser.scala
+++ parser.scala
@@ -2,44 +2,42 @@ import scala.util.parsing.combinator._
 import scala.util.parsing.input.Positional
 
 abstract class Context extends Positional
-case class AssignmentContext(identifiers: List[String], values: List[ExpressionContext]) extends Context
-case class CircuitCallContext(name: String, arguments: List[ExpressionContext]) extends Context
+case class AssignmentContext(variables: List[String], values: List[ExpressionContext]) extends Context
 case class CircuitDeclarationContext(
   name: String,
   paramsOpt: Option[List[String]],
   assignments: List[AssignmentContext],
   output: OutStatementContext
 ) extends Context
-case class FactorContext(
-  booleanValueOpt: Option[Boolean],
-  identifierOpt: Option[String],
-  circuitCallOpt: Option[CircuitCallContext],
-  expressionOpt: Option[ExpressionContext]
-) extends Context
 case class ModuleContext(decs: List[CircuitDeclarationContext]) extends Context
 case class OutStatementContext(outputs: List[ExpressionContext]) extends Context
 
 abstract class ExpressionContext extends Context
-case class FactorExpression(factor: FactorContext) extends ExpressionContext
-case class NotExpression(exp: ExpressionContext) extends ExpressionContext
-case class AndExpression(exp1: ExpressionContext, exp2: ExpressionContext) extends ExpressionContext
-case class NandExpression(exp1: ExpressionContext, exp2: ExpressionContext) extends ExpressionContext
-case class XorExpression(exp1: ExpressionContext, exp2: ExpressionContext) extends ExpressionContext
-case class XnorExpression(exp1: ExpressionContext, exp2: ExpressionContext) extends ExpressionContext
-case class OrExpression(exp1: ExpressionContext, exp2: ExpressionContext) extends ExpressionContext
-case class NorExpression(exp1: ExpressionContext, exp2: ExpressionContext) extends ExpressionContext
+type EC = ExpressionContext
 
-class BoolexParser extends JavaTokenParsers {
+case class NotExpression(exp: EC) extends ExpressionContext
+case class AndExpression(exp1: EC, exp2: EC) extends ExpressionContext
+case class NandExpression(exp1: EC, exp2: EC) extends ExpressionContext
+case class XorExpression(exp1: EC, exp2: EC) extends ExpressionContext
+case class XnorExpression(exp1: EC, exp2: EC) extends ExpressionContext
+case class OrExpression(exp1: EC, exp2: EC) extends ExpressionContext
+case class NorExpression(exp1: EC, exp2: EC) extends ExpressionContext
+case class BooleanValue(value: Boolean) extends ExpressionContext
+case class Variable(name: String) extends ExpressionContext
+case class CircuitCallContext(name: String, arguments: List[ExpressionContext]) extends ExpressionContext
+
+
+class BoolexParser extends JavaTokenParsers with PackratParsers {
   override protected val whiteSpace = """(\s|#.*)+""".r
 
-  def module: Parser[ModuleContext] = positioned(
-    rep(circuitDeclaration) ^^ (decs => ModuleContext(decs))
+  lazy val module: PackratParser[ModuleContext] = positioned(
+    rep1(circuitDeclaration) ^^ (decs => ModuleContext(decs))
   )
   
-  def circuitDeclaration: Parser[CircuitDeclarationContext] = positioned((
-      "circuit" ~! ident ~! opt("(" ~! rep1sep(ident, ",") ~! ")") ~!
-        rep(assignment) ~!
-        outStatement ~!
+  lazy val circuitDeclaration: PackratParser[CircuitDeclarationContext] = positioned((
+      "circuit" ~ ident ~ opt("(" ~ rep1sep(ident, ",") ~ ")") ~
+        rep(assignment) ~
+        outStatement ~
       "end"
     ) ^^ {
       case "circuit"~name~Some("("~(params)~")")~body~out~"end" =>
@@ -48,77 +46,86 @@ class BoolexParser extends JavaTokenParsers {
     }
   )
   
-  def assignment: Parser[AssignmentContext] = positioned(
-    rep1sep(ident, ",") ~! "=" ~! rep1sep(expression, ",") ^^ {
-      case identifiers~"="~values => AssignmentContext(identifiers, values)
+  lazy val assignment: PackratParser[AssignmentContext] = positioned(
+    rep1sep(ident, ",") ~ "=" ~ rep1sep(l1expression, ",") ^^ {
+      case variables~"="~values => AssignmentContext(variables, values)
     }
   )
   
-  def outStatement: Parser[OutStatementContext] = positioned(
-    "out" ~! rep1sep(expression, ",") ^^ {
+  lazy val outStatement: PackratParser[OutStatementContext] = positioned(
+    "out" ~ rep1sep(l1expression, ",") ^^ {
       case "out"~outputs => OutStatementContext(outputs)
     }
   )
 
-  def circuitCall: Parser[CircuitCallContext] = positioned(
-    ident ~! "(" ~! repsep(expression, ",") ~! ")" ^^ {
+  lazy val circuitCall: PackratParser[CircuitCallContext] = positioned(
+    ident ~ "(" ~ repsep(l1expression, ",") ~ ")" ^^ {
       case name~"("~args~")" => CircuitCallContext(name, args)
     }
   )
 
-  def factor: Parser[FactorContext] = positioned((
-      "(" ~! expression ~! ")"
-      | circuitCall
-      | ident
-    ) ^^ {
-      case "true" => FactorContext(Some(true), None, None, None)
-      case "false" => FactorContext(Some(false), None, None, None)
-      case ident: String => FactorContext(None, Some(ident), None, None)
-      case circuitCall: CircuitCallContext => FactorContext(None, None, Some(circuitCall), None)
-      case expression: ExpressionContext => FactorContext(None, None, None, Some(expression))
+  lazy val l1expression: PackratParser[ExpressionContext] = positioned(
+    ( (l1expression ~ ("xor"|"^") ~ l1expression)
+    | (l1expression ~ "xnor" ~ l1expression)
+    | l2expression
+  ) ^^ {
+      case (exp1: EC)~"xor"~(exp2: EC) => XorExpression(exp1, exp2)
+      case (exp1: EC)~"^"~(exp2: EC) => XorExpression(exp1, exp2)
+      case (exp1: EC)~"xnor"~(exp2: EC) => XnorExpression(exp1, exp2)
+      case (exp: EC) => exp
     }
   )
 
-  def expression: Parser[ExpressionContext] = { type EC = ExpressionContext; positioned((
-      factor
-      | expression ~! "\'"
-      | ("not" | "-") ~! expression
-      | expression ~! ("and" | "nand") ~! expression
-      | expression ~! ("xor" | "xnor") ~! expression
-      | expression ~! ("or" | "nor") ~! expression
-    ) ^^ {
-      case factor: FactorContext => FactorExpression(factor)
+  lazy val l2expression: PackratParser[ExpressionContext] = positioned(
+    ( (l2expression ~ ("or"|"+") ~ l2expression)
+    | (l2expression ~ "nor" ~ l2expression)
+    | l3expression
+  ) ^^ {
+      case (exp1: EC)~"or"~(exp2: EC) => OrExpression(exp1, exp2)
+      case (exp1: EC)~"+"~(exp2: EC) => OrExpression(exp1, exp2)
+      case (exp1: EC)~"nor"~(exp2: EC) => NorExpression(exp1, exp2)
+      case (exp: EC) => exp
+    }
+  )
+
+  lazy val l3expression: PackratParser[ExpressionContext] = positioned(
+    ( (l3expression ~ ("and"|"*") ~ l3expression)
+    | (l3expression ~ "nand" ~ l3expression)
+    | l4expression
+  ) ^^ {
+      case (exp1: EC)~"and"~(exp2: EC) => AndExpression(exp1, exp2)
+      case (exp1: EC)~"*"~(exp2: EC) => AndExpression(exp1, exp2)
+      case (exp1: EC)~"nand"~(exp2: EC) => NandExpression(exp1, exp2)
+      case (exp: EC) => exp
+    }
+  )
+
+  lazy val l4expression: PackratParser[ExpressionContext] = positioned(
+    ( (l4expression ~ "\'")
+    | (("not" | "-") ~ l4expression)
+    | l5expression
+  ) ^^ {
       case (exp: EC)~"\'" => NotExpression(exp)
       case "not"~(exp: EC) => NotExpression(exp)
       case "-"~(exp: EC) => NotExpression(exp)
-      case (exp1: EC)~"and"~(exp2: EC) => AndExpression(exp1, exp2)
-      case (exp1: EC)~"nand"~(exp2: EC) => NandExpression(exp1, exp2)
-      case (exp1: EC)~"xor"~(exp2: EC) => XorExpression(exp1, exp2)
-      case (exp1: EC)~"xnor"~(exp2: EC) => XnorExpression(exp1, exp2)
-      case (exp1: EC)~"or"~(exp2: EC) => OrExpression(exp1, exp2)
-      case (exp1: EC)~"nor"~(exp2: EC) => NorExpression(exp1, exp2)
+      case (exp: EC) => exp
     }
-  )}
+  )
+
+  lazy val l5expression: PackratParser[ExpressionContext] = positioned(
+    ( "(" ~> l1expression <~ ")"
+    | "true"
+    | "false"
+    | ident <~ not("(")
+    | circuitCall
+  ) ^^ {
+      case "true" => BooleanValue(true)
+      case "false" => BooleanValue(false)
+      case ident: String => Variable(ident)
+      case exp: EC => exp
+    }
+  )
 }
 
-val myCircuit = """
-x = 5
-"""
-
 val parser = new BoolexParser()
-println(parser.parseAll(parser.assignment, myCircuit))
-
-// # This RS latch is properly handled.
-// circuit RSLatch(r,s)
-//     P = r nor Q
-//     Q = s nor P
-//     out P, Q
-// end
-
-// circuit DLatch(d,clk)
-//     out RSLatch(d' and clk, d and clk)
-// end
-
-// circuit main(d,clk)
-//     out DLatch(d,clk)
-// end
+println(parser.parseAll(parser.module, myCircuit))
