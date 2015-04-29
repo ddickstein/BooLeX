4bb27eac9d7407ab9f065de6f5c5e664a40bf4f3
Dani Dickstein <danidickstein@yahoo.com>
2015-04-29 01:04:56 -0400

    change from RS to SR

--- src/main/scala/specs.scala
+++ src/main/scala/specs.scala
@@ -1,13 +1,13 @@
 object CircuitDemo { val specification = """
-# This RS latch is properly handled.
-circuit RSLatch(r,s)
+# This SR latch is properly handled.
+circuit SRLatch(s,r)
   P = r nor Q
   Q = s nor P
   out P, Q
 end
 
 circuit DLatch(d,clk)
-  out RSLatch(d' and clk, d and clk)
+  out SRLatch(d and clk, d' and clk)
 end
 
 circuit main(d)
